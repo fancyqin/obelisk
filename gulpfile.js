2cd15369e9d66f285cbf915e6ae81694e9613acd
fancyqin <fancarter007@163.com>
2016-09-26 23:07:13 +0800

    ss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var replace = require('gulp-replace');
+// var replace = require('gulp-replace');
 
 gulp.task('sass', function () {
     return sass('src/css/**/*.scss',{sourcemap:true})

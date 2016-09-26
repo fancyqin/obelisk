ce26fd2d449773e1e5edb7c7d5313f4f21c97d26
Fan <fancarter007@163.com>
2016-09-26 17:45:02 +0800

    del

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var replace = require('gulp-replace');
 
 gulp.task('sass', function () {
     return sass('src/css/**/*.scss',{sourcemap:true})
+    
         .on('error', sass.logError)
+
         // For inline sourcemaps
         .pipe(sourcemaps.write())
-
         // For file sourcemaps
         .pipe(sourcemaps.write('maps', {
             includeContent: false,

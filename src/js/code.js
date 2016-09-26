9ca34fb90ebbaf98adbbe2f7e2c38babcec04e67
Fan <fancarter007@163.com>
2016-09-26 15:18:11 +0800

    :octocat:

--- src/js/code.js
+++ src/js/code.js
@@ -7,19 +7,19 @@ function qsa(sel) {
 }
 
 if (!Array.prototype.forEach){
- Array.prototype.forEach = function(fun /*, thisp*/)
- {
-  var len = this.length;
-  if (typeof fun != "function")
-   throw new TypeError();
- 
-  var thisp = arguments[1];
-  for (var i = 0; i < len; i++)
-  {
-   if (i in this)
-    fun.call(thisp, this[i], i, this);
-  }
- };
+    Array.prototype.forEach = function(fun /*, thisp*/)
+        {
+        var len = this.length;
+        if (typeof fun != "function")
+        throw new TypeError();
+
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++)
+        {
+        if (i in this)
+        fun.call(thisp, this[i], i, this);
+        }
+    };
 }
 
 
@@ -51,6 +51,7 @@ if (!Array.prototype.forEach){
         editor.setAutoScrollEditorIntoView(false);
         editor.setOption("minLines", 2);
         editor.setOption("maxLines", 100);
+        editor.$blockScrolling = Infinity;
 
         editor.setValue(box.innerHTML);
         
